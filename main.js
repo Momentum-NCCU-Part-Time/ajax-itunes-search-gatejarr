528f576286e1dcda3945d04ee3fc5558104a19ad
gatejarr <gatejarr@gmail.com>
2023-11-08 16:29:46 -0500

    WIP; started code. Currently returning empty array

---
+++ main.js
@@ -0,0 +1,29 @@
+const iTunesUrl = "https://itunes.apple.com/search?";
+let searchForm = document.getElementById("searchForm");
+let searchTerm = document.getElementById("searchField");
+let button = document.getElementById("searchButton");
+let results = document.getElementById("results");
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let term = searchTerm.value;
+    console.log(term);
+    fetch(iTunesUrl + term).then((response) => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            let errorMsg = document.createElement ('h2');
+            errorMsg.innerText = "API call failed, please try again.";
+            results.appendChild(errorMsg);
+        }
+    }).then((parsedJsonResponse) => {
+        console.log(iTunesUrl + term);
+        console.log(response);
+        //console.log(parsedJsonResponse);
+    })
+}) 
+
+// Search address needs to look like: https://itunes.apple.com/search?term=Madonna
+// https://itunes.apple.com/search?term=jack+johnson
+
+//currently returning empty arrays
