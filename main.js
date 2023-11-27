ea20739cbed56fa79371c9177a6e9a29c0bc3d26
Jarrod <gatejarr@gmail.com>
2023-11-27 17:31:59 -0500

    working on getting previews to play with song title showing

--- main.js
+++ main.js
@@ -5,116 +5,52 @@ let searchTerm = document.getElementById("searchField");
 let button = document.getElementById("searchButton");
 let container = document.getElementById("container");
 let preview = document.getElementById("preview");
-//let previewButton = document.getElementById("previewButton");
 
- searchForm.addEventListener('submit', (event) => {
-     event.preventDefault();
-     let term = searchTerm.value;
-     console.log(term);
-     fetch(iTunesUrl + term + limit, {
-         method: "GET",
-         headers: {"Content-Type": "application/json"},
-     }).then((response) => {
-         if (response.status === 200) {
-             return response.json();
-         } else {
-             let errorMsg = document.createElement ('h2');
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let field = [];
+    field = searchField.value;
+  fetch(iTunesUrl +field + "&media=music" + limit).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+    else {
+      let errorMsg = document.createElement ('h2');
              errorMsg.innerText = "API call failed, please try again.";
              container.appendChild(errorMsg);
-         }
-     }).then((parsedJsonResponse) => {
-         console.log(parsedJsonResponse);
-         const songs = parsedJsonResponse.results;
-         if(parsedJsonResponse.resultCount === 0) {
-            alert('No Results found. Please try again.')
-         }else{
-         let songTracks = songs.map(results => {
-         // this is in a loop
-            preview.innerHTML = `
-             <figure>
-                 <figcaption id="previewTrackName">Preview:</figcaption>
-                 <audio controls src=""></audio>
-             </figure>`,
-             container.innerHTML += `
-             <div class="songTile">
-                 <img src=${results.artworkUrl100} />
-                 <button class="previewButton" data-id=${results.trackId}>${results.trackName}</button>
-                 <h3>${results.artistName}</h3>
-              </div>
-                 `
+    }
+  }).then((parsedJsonResponse) => {
+    if(parsedJsonResponse.resultCount === 0) {
+      alert('No Results found. Please try again.')
+   }else {
+    console.log(parsedJsonResponse);
+    const songs = parsedJsonResponse.results;
+    songs.innerHTML = [];
+    return songs.map(results => {
+      const songCard = document.createElement('div');
+      artist = document.createElement('h3');
+      songTitle = document.createElement('p');
+      albumArt = document.createElement('img');
+      let previewButton = document.createElement('button');
+      
+      //Song Preview
+      previewButton.innerText = "Play Preview";
+      previewButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        preview.src = results.previewUrl;
+      });
 
-            //Break out into own function outside of fetch request and call the function
-            //Should loop through all preview buttons
-        //  }),let previewButtons = document.querySelectorAll(".previewButton");
-        //  for (let button of previewButtons) {
-        //      button.addEventListener("click", (e) =>{
-        //      event.preventDefault()
-        //      let previewId = results.trackId;
-        //      console.log(previewId);
-          })
-        //  }
-        }
-      })
- })
-// previewButton.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     let previewId = dataset.trackId;
-//     console.log(previewId);
-// })
+      artist.innerHTML = results.artistName;
+      songTitle.innerHTML = results.trackName;
+      albumArt.src = results.artworkUrl100;
 
+      songCard.appendChild(albumArt);
+      songCard.appendChild(songTitle);
+      songCard.appendChild(artist);
+      songCard.appendChild(previewButton);
 
-
-
-//  songElement.addEventListener("click", function (event) {
-//     if (event.target.textContent.includes(song.trackName)) {
-//         audioPlayer.src = event.target.nextElementSibling.innerText
-//     }
-// })
-
-
-
-
-// DIFFERENT WAY OF FORMATING JS
-
-// const app = {
-//     data: {
-//         url: "https://itunes.apple.com/search?term=Madonna",
-//         songTiles: []
-//     },
-
-//     searchForm.addEventListener('submit', (event) => {
-
-//     })
-
-//     getSongs: function () {
-//         this.data.songTiles = []
-//         fetch(this.data.url + term, {
-//             method: "GET",
-//             headers: {"Content-Type": "application/json"},
-//         })
-//         .then(r => r.json())
-//         .then(response => {
-//             this.data.songTiles = []
-//            for (let songs of response.results){ 
-//             this.data.songTiles.push(songs)
-//            }
-//            this.generateTilesHTML();
-//         });
-//     },
-
-//     generateTilesHTML: function () {
-//         let container = document.getElementById("container");
-//         for (let songs of this.data.songTiles) {
-//             container.innerHTML += `
-//             <div class ="songTile">
-//                 <img src=${songs.artworkUrl100}
-//                 <h6>${songs.trackName}</h6>
-//                 <h3>${songs.artistName}</h3>
-//                 </div>`
-//         }
-//     },
-
-//     main: function () {
-//         this.searchButton();
-//     },
-// };
+      container.appendChild(songCard);
+    })
+   }
+  })
+})
